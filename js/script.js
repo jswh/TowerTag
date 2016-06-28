67ed87aeb787b21d170064d8201dc368c88a7d2a
jswh <jswh@jswh.me>
2016-06-28 16:19:07 +0800

    color picker

--- js/script.js
+++ js/script.js
@@ -1,66 +1,92 @@
 (function () {
-	chrome.storage.sync.get(function(setting) {
-		$(document).ready(function() {
-			addCss();
-			changeAll();
+	$(document).ready(function () {
+		addCss();
+		chrome.storage.sync.get(function(setting) {
+			changeAll(setting);
 		});
+
 		$(document).on('DOMNodeInserted', function(e) {
+			chrome.storage.sync.get(function(setting) {
 			if(e.target.className == 'todos-all todos-view member-view' || e.target.className == 'todos-all todos-view list-view' || e.target.className == 'simple-filedrop') {
-				changeAll();
+				changeAll(setting);
 			}
+			});
 		});
 
 		$('#todolists').on('DOMNodeInserted', function (e) {
-			target = $(e.target);
-			if(target.hasClass('todo')) {
-				change(target.find('.content-linkable>.tag'));
-			}
-		});
-
-		function changeAll() {
-			$('.content-linkable>.tag').each(function (i, tag) {
-				$tag = $(tag);
-				change($tag);
-			});
-		}
-
-		function addCss() {
-			$('head').append("<style>.block-header .simple-checkbox, .block-header .todo-detail{display:none;}.block-header .todo-content{color:#333}</style>");
-		}
-
-		function change($tag) {
-				console.log($tag);
-				tagText = $tag.text();
-				console.log(tagText);
-				switch (tagText) {
-					case 'block-header':
-						$todo = $tag.closest('.todo');
-						$todo.css({
-							'margin':'10px 0',
-							'text-align':'center'
-						});
-						$wrap = $tag.closest('.todo-wrap');
-						$wrap.addClass('block-header');
-						$tag.hide();
-						break;
-					default:
-						if (setting[tagText]) {
-							changeColor($tag, setting[tagText]);
-						}
+			chrome.storage.sync.get(function(setting) {
+				target = $(e.target);
+				if(target.hasClass('todo')) {
+					change(target.find('.content-linkable>.tag'), setting);
 				}
-		}
-
-		function changeColor($tag, color) {
-			$tag.removeClass('tag');
-			$tag.css({
-				'background': color,
-				'color': '#fff',
-				'padding': '0.1em 0.2em',
-				'vertical-align': '1px',
-				'font-size': '82%',
-				'font-weight': 'normal',
-				'border-radius': '2px'
 			});
-		}
+		});
 	});
+
+	function changeAll(setting) {
+		$('.content-linkable>.tag').each(function (i, tag) {
+			$tag = $(tag);
+			change($tag, setting);
+		});
+		$('.content-linkable>.changed-tag').each(function (i, tag) {
+			$tag = $(tag);
+			change($tag, setting);
+		});
+	}
+
+	function addCss() {
+		$('head').append("<style>.block-header .simple-checkbox, .block-header .todo-detail{display:none;}.block-header .todo-content{color:#333}</style>");
+	}
+
+	function change($tag, setting) {
+			tagText = $tag.text();
+			switch (tagText) {
+				case 'block-header':
+					$todo = $tag.closest('.todo');
+					$todo.css({
+						'margin':'10px 0',
+						'text-align':'center'
+					});
+					$wrap = $tag.closest('.todo-wrap');
+					$wrap.addClass('block-header');
+					$tag.hide();
+					break;
+				default:
+					if (setting[tagText]) {
+						changeColor($tag, setting[tagText]);
+					} else if ($tag.hasClass('change-tag')) {
+						revertColor($tag);
+					}
+			}
+	}
+
+	function changeColor($tag, color) {
+		$tag.removeClass('tag');
+		$tag.addClass('changed-tag');
+		$tag.css({
+			'background': color.bg,
+			'color': color.ft,
+			'padding': '0.1em 0.2em',
+			'vertical-align': '1px',
+			'font-size': '82%',
+			'font-weight': 'normal',
+			'border-radius': '2px'
+		});
+	}
+
+	function revertColor($tag) {
+		$tag.css({
+			'background': color.bg,
+			'color': color.ft,
+			'padding': '0.1em 0.2em',
+			'vertical-align': '1px',
+			'font-size': '82%',
+			'font-weight': 'normal',
+			'border-radius': '2px'
+		});
+		$tag.addClass('tag');
+		$tag.removeClass('changed-tag');
+	}
+
 })();
+function test() {console.log(123);}
--- js/setting.js
+++ js/setting.js
@@ -2,28 +2,33 @@ $(document).ready(function () {
 	var form = $('form');
 	chrome.storage.sync.get(function(setting) {
 		for (var i in setting) {
-			form.append(makeLine(i, setting[i]));
+			$line = $(makeLine(i, setting[i].bg, setting[i].ft));
+			form.append($line);
+			bindColorPicker($line);
 		}
 	});
 
 	$('#add').click(function () {
-		form.append(makeLine('', ''));
+		$line = $(makeLine('', '', ''));
+		form.append($line);
+		bindColorPicker($line);
 	});
 
-	$('form').focusout(function (evt) {
-		if($(evt.target).is('input')) {
-			save();
-		}
+	form.focusout(function () {
+		save();
 	});
 
-	function makeLine(name, color) {
-
-		return '<div class="tag-group">' + 
+	function makeLine(name, bg, ft) {
+		bg = bg ? bg : '#e3f2f7';
+		ft = ft ? ft : '#335566';
+		return '<div class="tag-group" style="text-align: center;">' + 
 			'<div class="input-field col s6" style="margin:0">' +
-				'<input placeholder="name" class="name" style="margin:0" type="text" value="' + name + '">' +
-			'</div>' +
+				'<input placeholder="name" class="name" style="margin:0;text-align:center;" type="text" value="' + name +
+		  '"></div>' +
 			'<div class="input-field col s6" style="margin:0">' +
-				'<input placeholder="color" class="color" style="margin:0" type="text" value="' + color + '">' +
+				'<span><a class="tag-sample" style="color:' + ft + ';background-color:' + bg + ';">' + name + '</a></span>' +
+				'<input style="display:none;" class="bg" type="text" style="margin:0" value="' + bg + '">' +
+				'<input style="display:none;" class="ft" type="text" style="margin:0" value="' + ft + '">' +
 			'</div>' +
 		'</div>';
 	}
@@ -34,9 +39,10 @@ $(document).ready(function () {
 		$(".tag-group").each(function (index, ele){
 			ele = $(ele);
 			name = ele.find('.name').val();
-			color = ele.find('.color').val();
-			if (name && color) {
-				setting[name] = color;
+			bg = ele.find('.bg').val();
+			ft = ele.find('.ft').val();
+			if (name && bg) {
+				setting[name] = {'bg':bg, 'ft':ft};
 			}
 		});
 		chrome.storage.sync.get(function(oldSetting) {
@@ -47,5 +53,26 @@ $(document).ready(function () {
 			}
 		});
 		chrome.storage.sync.set(setting);
+	}
+
+	fronted = false;
+	function bindColorPicker($line) {
+			ts = $line.find('.tag-sample');
+			$line.find('span').colorPicker({
+				renderCallback:function ($ele, toggled) {
+					if (toggled === undefined) {
+						bg = '#' + this.color.colors.HEX;
+						ft = this.color.colors.HUELuminance > 0.22 ? '#666' : '#fff';
+						ts.css('background-color', bg);
+						ts.css('color', ft);
+						$line.find('.bg').val(bg);
+						$line.find('.ft').val(ft);
+					} else if (toggled === false) {
+						save();
+					} else if (toggled === true && !fronted) {
+						$('.cp-color-picker').css('z-index', '1');
+					}
+				}
+			});
 	}
 });
