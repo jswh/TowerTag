9ee5373424144c3f0ff7d026bc6e6c8dc5bb4f43
jswh <jswh@jswh.me>
2016-06-28 18:12:34 +0800

    instance see

--- js/setting.js
+++ js/setting.js
@@ -19,22 +19,22 @@ $(document).ready(function () {
 	form.focusout(function () {
 		save();
 	});
-	
+
 	function makeLine(name, bg, ft) {
 		bg = bg ? bg : '#e3f2f7';
 		ft = ft ? ft : '#335566';
-		return '<div class="tag-group" style="text-align: center;">' + 
+		return '<div class="tag-group" style="text-align: center;">' +
 			'<div class="input-field col s6" style="margin:0">' +
-				'<input placeholder="name" class="name" style="margin:0;text-align:center;" type="text" value="' + name +
-		  '"></div>' +
+			'<input placeholder="name" class="name" style="margin:0;text-align:center;" type="text" value="' + name +
+			'"></div>' +
 			'<div class="input-field col s6" style="margin:0">' +
-				'<span><a class="tag-sample" style="color:' + ft + ';background-color:' + bg + ';">' + name + '</a></span>' +
-				'<input style="display:none;" class="bg" type="text" style="margin:0" value="' + bg + '">' +
-				'<input style="display:none;" class="ft" type="text" style="margin:0" value="' + ft + '">' +
+			'<span><a class="tag-sample" style="color:' + ft + ';background-color:' + bg + ';">' + name + '</a></span>' +
+			'<input style="display:none;" class="bg" type="text" style="margin:0" value="' + bg + '">' +
+			'<input style="display:none;" class="ft" type="text" style="margin:0" value="' + ft + '">' +
 			'</div>' +
-		'</div>';
+			'</div>';
 	}
-	
+
 
 	function save() {
 		var setting = {};
@@ -59,23 +59,25 @@ $(document).ready(function () {
 
 	fronted = false;
 	function bindColorPicker($line) {
-			ts = $line.find('.tag-sample');
-			$line.find('span').colorPicker({
-				renderCallback:function ($ele, toggled) {
-					if (toggled === undefined) {
-						bg = '#' + this.color.colors.HEX;
-						ft = this.color.colors.HUELuminance > 0.22 ? '#666' : '#fff';
-						ts.css('background-color', bg);
-						ts.css('color', ft);
-						$line.find('.bg').val(bg);
-						$line.find('.ft').val(ft);
-					} else if (toggled === false) {
-						save();
-					} else if (toggled === true && !fronted) {
-						$('.cp-color-picker').css('z-index', '1');
-					}
+		ts = $line.find('.tag-sample');
+		$line.find('span').colorPicker({
+			customBG: '#000',
+			margin: '7px 0 0 0',
+			renderCallback:function ($ele, toggled) {
+				if (toggled === undefined) {
+					bg = '#' + this.color.colors.HEX;
+					ft = this.color.colors.HUELuminance > 0.22 ? '#666' : '#fff';
+					ts.css('background-color', bg);
+					ts.css('color', ft);
+					$line.find('.bg').val(bg);
+					$line.find('.ft').val(ft);
+				} else if (toggled === false) {
+					save();
+				} else if (toggled === true && !fronted) {
+					$('.cp-color-picker').css('z-index', '1');
 				}
-			});
+			},
+		});
 	}
 
 	function bindNameChange($line) {
